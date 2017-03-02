a1a5385f557c613e5b8ed4dbcb6b991d1b722607
dxs <glaglasven@live.com>
2017-03-02 22:43:17 +0100

    first shit

--- QR/MainPage.xaml.cs
+++ QR/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QR.Logic;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +26,8 @@ namespace QR
         public MainPage()
         {
             this.InitializeComponent();
+			Scan s = new Scan();
+			s.TakePicture();
         }
     }
 }
